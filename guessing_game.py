9ead285150c0c8be42b0c2f590c6d87f2777e1a7
Hadiah Venner <30780014+hkvenner@users.noreply.github.com>
2021-12-03 15:48:17 -0500

    updated read me

--- guessing_game.py
+++ guessing_game.py
@@ -8,27 +8,31 @@ import random, statistics
 
 def start_game():
   print("Hi there. You're about to play an epic Word Guessing Game.")
-  answer = random.randint(1,10)
+  answer = random.randint(1,100)
   attempts = 0
   attempts_list = []
   
   while True:
-    try:
-      guess = int(input("Please pick a number between 1 and 10: "))      
+    try:      
+      guess = int(input("Please pick a number between 1 and 100: "))
+      if (guess < 1) or (guess > 100):
+        print("Your number was out of the desired range")
+        continue
     except ValueError:
       print("You didn't enter a valid number")
       continue
     attempts += 1
-    attempts_list.append(guess)
+    
     if answer == guess: 
-        print("You've got it! Your guess was right on target.It took you ", attempts, " attempts.")
-        #attempts_list.append(attempts)
+        print("You've got it! Your guess was right on target.It took you {} attempts.".format(attempts))
+        attempts_list.append(attempts)
         play_again = input("Would you like to play again? (yes/no) ")
-        if play_again == "yes":
-          answer = random.randint(1,10)
+        if play_again.lower() == "yes":
+          print("The number of least guesses (i.e the goal you are trying to beat) is {}".format(min(attempts_list)))
+          answer = random.randint(1,100)
           attempts = 0
           continue
-        elif play_again == "no":
+        elif play_again.lower() == "no":
           print("No worries. Thanks for playing!")
           break
     elif answer < guess :
@@ -37,34 +41,14 @@ def start_game():
     elif answer > guess : 
         print("It's higher")
         continue
-    #guess = input("Your answer wasn't quite right  :( Please pick another number between 1 and 10: ")
+        
   mean = statistics.mean(attempts_list)
   mode = statistics.mode(attempts_list)
   median = statistics.median(attempts_list)
   print(attempts_list)
-  print("The number of attempts you made was ", attempts, "\nThe mean was ", round(mean),"\nThe median was ", round(median),"\nThe mode was ", round(mode))
+  print("The number of attempts you made was {} attempts.".format(attempts))
+  print("The mean was {}.".format(round(mean)))
+  print("The median was {}.".format(round(median)))
+  print("The mode was {}.".format(round(mode)))
 
-  #print(answer)
-  
-    # write your code inside this function.
-"""Psuedo-code Hints
-    
-    When the program starts, we want to:
-    ------------------------------------
-    1. Display an intro/welcome message to the player. (DONE)
-    2. Store a random number as the answer/solution.
-    3. Continuously prompt the player for a guess.
-      a. If the guess greater than the solution, display to the player "It's lower".
-      b. If the guess is less than the solution, display to the player "It's higher".
-    
-    4. Once the guess is correct, stop looping, inform the user they "Got it"
-         and show how many attempts it took them to get the correct number.
-    5. Save their attempt number to a list.
-    6. At the end of the game, show the player, 1) their number of attempts, 2) the mean, median, and mode of the saved attempts list.
-    7. Ask the player if they want to play again.
-    ( You can add more features/enhancements if you'd like to. )
-    """
-
-
-# Kick off the program by calling the start_game function.
 start_game()
